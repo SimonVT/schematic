5f2ccad6713d0fb82ff685c6731697b42c8b445f
Simon Vig Therkildsen <simonvt@gmail.com>
2014-05-16 02:56:06 +0200

    Remove unused code.

--- schematic-compiler/src/main/java/net/simonvt/schematic/compiler/SchematicProcessor.java
+++ schematic-compiler/src/main/java/net/simonvt/schematic/compiler/SchematicProcessor.java
@@ -16,13 +16,9 @@
 
 package net.simonvt.schematic.compiler;
 
-import com.squareup.javawriter.JavaWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.lang.annotation.Annotation;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -34,12 +30,10 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedOptions;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 import javax.tools.Diagnostic.Kind;
-import javax.tools.JavaFileObject;
 import net.simonvt.schematic.annotation.ContentProvider;
 import net.simonvt.schematic.annotation.Database;
 
@@ -97,23 +91,10 @@ public class SchematicProcessor extends AbstractProcessor {
     return true;
   }
 
-  static class DatabaseItem {
-    TypeElement database;
-    String name;
-
-    DatabaseItem(TypeElement database, String name) {
-      this.database = database;
-      this.name = name;
-    }
-  }
-
   private void processDatabases(RoundEnvironment env) {
-    List<DatabaseItem> databases = new ArrayList<DatabaseItem>();
-
     for (Element database : env.getElementsAnnotatedWith(Database.class)) {
       Database annotation = database.getAnnotation(Database.class);
       try {
-        databases.add(new DatabaseItem((TypeElement) database, annotation.className()));
         new DatabaseWriter(processingEnv, database, outPackage).writeJava(filer);
       } catch (IOException e) {
         throw new RuntimeException(e);
@@ -127,64 +108,6 @@ public class SchematicProcessor extends AbstractProcessor {
         e.printStackTrace();
         error("Unable to process " + provider.asType().getKind().name());
       }
-    }
-  }
-
-  private void writeSchematicInternal(List<DatabaseItem> databases) {
-    try {
-      JavaFileObject jfo = filer.createSourceFile("net.simonvt.schematic.Schematic");
-      Writer out = jfo.openWriter();
-      JavaWriter writer = new JavaWriter(out);
-
-      writer.emitPackage("net.simonvt.schematic");
-
-      writer.emitImports("android.content.Context")
-          .emitImports("android.database.sqlite.SQLiteOpenHelper")
-          .emitImports("java.util.HashMap")
-          .emitImports("java.util.Map");
-
-      writer.beginType("Schematic", "class", EnumSet.of(Modifier.FINAL));
-
-      writer.emitField("Map<String, String>", "DATABASE_IMPLS",
-          EnumSet.of(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL),
-          "new HashMap<String, String>()");
-
-      writer.beginInitializer(true);
-      for (DatabaseItem item : databases) {
-        String definitionName = elementUtils.getBinaryName(item.database).toString();
-        String implName = outPackage + item.name;
-        writer.emitStatement("DATABASE_IMPLS.put(%s, %s)", definitionName, implName);
-      }
-      writer.endInitializer().emitEmptyLine();
-
-      writer.emitField("Map<Class, SQLiteOpenHelper>", "DATABASES",
-          EnumSet.of(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL),
-          "new HashMap<Class, Class>()").emitEmptyLine();
-
-      writer.beginMethod("SQLiteOpenHelper", "getDatabase", EnumSet.of(Modifier.FINAL), "Context",
-          "context", "Class", "database");
-      writer.emitStatement("SQLiteOpenHelper openHelper = DATABASES.get(database)");
-      writer.beginControlFlow("if (openHelper != null)")
-          .emitStatement("return openHelper;")
-          .endControlFlow()
-          .emitEmptyLine();
-
-      writer.emitStatement("String databaseName = database.getSimpleName()")
-          .emitStatement("String implName = DATABASE_IMPLS.get(databaseName)")
-          .emitStatement("Class implClass = Class.forName(implName)")
-          .emitStatement("Method getInstance = implClass.getMethod(\"getInstance\", Context.class)")
-          .emitStatement("openHelper = (SQLiteOpenHelper) getInstance.invoke(null, context)")
-          .emitEmptyLine()
-          .emitStatement("DATABASES.put(database, openHelper)")
-          .emitEmptyLine()
-          .emitStatement("return openHelper");
-
-      writer.endMethod();
-
-      writer.endType();
-      writer.close();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
     }
   }
 
